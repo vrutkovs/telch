7d87b245bcdcbc20ffb3d0c4077b7c0c0e4084ed
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-04-07 11:57:53 +0200

    Implement a side bar with quick status-based filters

--- telch/telch.py
+++ telch/telch.py
@@ -19,7 +19,9 @@ async def on_shutdown(app):
 
 @template('home.jinja2')
 async def root(request):
-    return {'ws_url': request.app.router['ws'].url_for()}
+    return {
+        'ws_url': request.app.router['ws'].url_for(),
+        'active_filter': request.app['active_filter']}
 
 
 async def ws(request):
@@ -28,16 +30,16 @@ async def ws(request):
     await ws.prepare(request)
     async for msg in ws:
         if msg.tp == WSMsgType.text:
-            if msg.data == 'ready':
-                item_filter = INITIAL_FILTER
-            else:
-                item_filter = json.loads(msg.data)
+            if msg.data != 'ready':
+                request.app['active_filter'] = json.loads(msg.data)
 
-            response = render_template('filter.jinja2', request, {'filters': item_filter})
+            response = render_template('filter.jinja2', request,
+                                       {'filters': request.app['active_filter']})
             ws.send_str(json.dumps({'filter': response.text}))
 
-            for item in taskwarrior.get_tasks_matching(app.w, item_filter):
-                response = render_template('task.jinja2', request, {'task': item})
+            tasks = taskwarrior.get_tasks_matching(app.w, request.app['active_filter'])
+            for task in tasks:
+                response = render_template('task.jinja2', request, {'task': task})
                 ws.send_str(json.dumps({'task': response.text}))
 
             ws.send_str(json.dumps({'end': True}))
@@ -51,6 +53,7 @@ app.router.add_route('*', '/', root)
 app.router.add_route('*', '/ws', ws, name='ws')
 
 app['websockets'] = []
+app['active_filter'] = INITIAL_FILTER
 app.on_shutdown.append(on_shutdown)
 
 app.w = taskwarrior.get_taskwarrior_instance()
